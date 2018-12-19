c3d6b00d360ba5c13ca4d79a3a564ea0d59ee10c
DamSaulGoodMan <capron.damien@laposte.net>
2018-12-19 23:37:08 +0100

    -> Path error of second "main()" -> Reorganize main CMakeList.txt -> The app can run now :)

--- src/DonkeyKong.cpp
+++ src/DonkeyKong.cpp
@@ -1,8 +1,8 @@
 // DonkeyKong.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
-#include "pch.h"
-#include "Game.h"
+#include "../include/pch.h"
+#include "../include/Game.h"
 
 
 int main()
