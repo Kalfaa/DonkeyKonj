728d58c22120308f5dac574f62506342a085db8f
DamSaulGoodMan <capron.damien@laposte.net>
2018-12-27 16:15:27 +0100

    Ok, sprite load is done but their is some bug so don't use it...

--- src/game/SpritesSheet.cpp
+++ src/game/SpritesSheet.cpp
@@ -6,18 +6,40 @@
 
 using namespace std;
 
+std::map<const std::string, sf::IntRect> SpritesSheet::sprites;
+std::map<const std::string, std::vector<sf::IntRect>> SpritesSheet::spritesPatterns;
+
+int SpritesSheet::lastNumSprite = -1;
+
 bool SpritesSheet::loadSprites(string file)
 {
     std::map<std::string, std::array<int, 4>> map = loadSpriteSetting(file.substr(0, file.find_last_of('.') - 1));
 
-    sf::Event event;
     sf::Texture mainTexture;
-
     mainTexture.loadFromFile(file);
 
+    string lastSpriteName;
     for(const auto& tab : map)
     {
-        //sprites.insert(make_pair(tab.first, sf::Sprite(mainTexture, sf::IntRect(0,0,300,400))));
+        sf::IntRect rect(tab.second[0], tab.second[1], tab.second[2], tab.second[3]);
+        sprites.insert(make_pair(tab.first, rect));
+
+        size_t pos = isPattern(tab.first, lastSpriteName);
+        if(pos)
+        {
+            if(spritesPatterns.find(tab.first.substr(0, pos)) != spritesPatterns.end())
+            {
+                spritesPatterns.at(tab.first.substr(0, pos)).push_back(rect);
+            }
+            else
+            {
+                vector<sf::IntRect> vec;
+                vec.push_back(rect);
+                spritesPatterns.insert(make_pair(tab.first.substr(0, pos), vec));
+            }
+        }
+
+        lastSpriteName = tab.first;
     }
 
     return true;
@@ -40,4 +62,33 @@ std::map<std::string, std::array<int, 4>> SpritesSheet::loadSpriteSetting(std::s
         mapSettings.insert(pair<string, array<int, 4>>(spriteName, tab));
     }
     return map<string, array<int, 4>>();
+}
+
+size_t SpritesSheet::isPattern(const string currentSprite, const string lastSprite)
+{
+    int numSprite = lastNumSprite + 1;
+
+    // Test if sprite name end with lastNumSprite + 1
+    size_t testEndWithNum = currentSprite.find_last_of(to_string(numSprite));
+    if(testEndWithNum == string::npos)
+    {
+        lastNumSprite = -1;
+        return 0;
+    }
+
+    // Test if sprite name is equal to the last sprite name (without last num) and if there is one last
+    if(lastNumSprite > -1 && !lastSprite.empty())
+    {
+        string spriteNameWithoutNum = lastSprite.substr(0, lastSprite.find_last_of(to_string(numSprite - 1)));
+
+        if(spriteNameWithoutNum != currentSprite.substr(0, testEndWithNum))
+        {
+            lastNumSprite = -1;
+            return 0;
+        }
+    }
+
+    lastNumSprite++;
+
+    return testEndWithNum;
 }
