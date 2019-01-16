82af272ef4b4d08d031e1545284958ffa46f5e1e
Kalfa <theo92320@hotmail.fr>
2019-01-17 00:00:33 +0100

    merge j'espere ca a pas tout K C

--- src/game/entities/Player.cpp
+++ src/game/entities/Player.cpp
@@ -18,8 +18,7 @@ Player::Player(const sf::Sprite &sprite, const sf::Vector2f &posPlayer, EntityTy
 
 }
 
-void Player::update(sf::Time elapsedTime, Map map)
-{
+void Player::update(sf::Time elapsedTime, Map map) {
 
     sf::Vector2f grindLadder(0.f, -playerSpeed * 2); // en attendant de trouver une maniere plus propre
     sf::Vector2f moveJump(0.f, -MARIO_JUMP_SPEED);
@@ -29,25 +28,17 @@ void Player::update(sf::Time elapsedTime, Map map)
     sf::Vector2f moveUp(0.f, -playerSpeed);
     sf::Vector2f moveRight(playerSpeed, 0.f);
     sf::Vector2f moveLeft(-playerSpeed, 0.f);
-    if (playerState != JUMP && playerState != STARTJUMP)
-    {
+    if (playerState != JUMP && playerState != STARTJUMP) {
         sprite.move(moveDown * elapsedTime.asSeconds());
-        if (!collide(map, EntityType::PLATFORM, DOWN))
-        {
+        if (!map.collide(sprite, EntityType::PLATFORM, DOWN)) {
             playerState = FALLING;
-        } else
-        {
-        if(!map.collide(sprite,EntityType::PLATFORM,DOWN )){
-            playerState=FALLING;
-        }else{
+        } else {
             sprite.move(moveUp * elapsedTime.asSeconds());
             playerState = IDLE;
         }
-    } else
-    {
+    } else {
         //sprite.move(0.f,-playerSpeed);
-        if (playerState == STARTJUMP)
-        {
+        if (playerState == STARTJUMP) {
             printf("jump");
             playerState = JUMP;
             jumpValue = MARIO_JUMP_MAX;
@@ -56,104 +47,83 @@ void Player::update(sf::Time elapsedTime, Map map)
 
     switch (direction) {
         case UP:
-            if (collide(map, EntityType::LADDER, DOWN))
-            {
-            if (map.collide(sprite, EntityType::LADDER, DOWN)) {
-                sprite.move(grindLadder * elapsedTime.asSeconds());
-            }
-            break;
-        case DOWN:
-            /*if(!collide(map,EntityType::PLATFORM,DOWN))
-                movement.y += playerSpeed;*/
-            break;
-        case LEFT:
-        {
-            if (lastDirection != LEFT)
-            {
-                TimeAnimation = 0;
-            } else
-            {
-            } else {
-                TimeAnimation += elapsedTime.asMilliseconds();
-            }
-            if (playerState != FALLING)
-            {
-            if (playerState != FALLING) {
-
-                changeSprite(updateAnimation(&TimeAnimation, 100, spritesPtns.at(movePatternLeft)));
-            } else
-            {
-                changeSprite(updateAnimation(&TimeAnimation, 100, spritesPtns.at(movePatternLeft)));
-            } else {
-                changeSprite(spritesPtns.at(movePatternLeft)[0]);
-            }
-            //printf("%d |",elapsedTime.asMilliseconds());
-            sprite.move(moveLeft * elapsedTime.asSeconds());
-            if (map.collide(sprite, EntityType::PLATFORM, RIGHT))sprite.move(moveRight * elapsedTime.asSeconds());
-            lastDirection = LEFT;
-            break;
-        }
-        case RIGHT:
-            if (lastDirection != RIGHT)
-            {
-                TimeAnimation = 0;
-            } else
-            {
-            } else {
-                TimeAnimation += elapsedTime.asMilliseconds();
-            }
-            //printf("%d |",elapsedTime.asMilliseconds());
-            if (playerState != FALLING)
-            {
-            if (playerState != FALLING) {
-
-                changeSprite(updateAnimation(&TimeAnimation, 100, spritesPtns.at(movePatternRight)));
-            } else
-            {
-            } else {
-                changeSprite(spritesPtns.at(movePatternRight)[0]);
-            }
-            sprite.move(moveRight * elapsedTime.asSeconds());
-            if (map.collide(sprite, EntityType::PLATFORM, RIGHT))sprite.move(moveLeft * elapsedTime.asSeconds());
-            if (collide(map, EntityType::PLATFORM, RIGHT))sprite.move(moveLeft * elapsedTime.asSeconds());
-            lastDirection = RIGHT;
-            break;
-        default:
-            if (playerState != JUMP && playerState != STARTJUMP && playerState != FALLING){
-
-                if (lastDirection == LEFT) {
-
+            if (collide(map, EntityType::LADDER, DOWN)) {
+                if (map.collide(sprite, EntityType::LADDER, DOWN)) {
+                    sprite.move(grindLadder * elapsedTime.asSeconds());
                 }
-            if (lastDirection == LEFT)
-            {
-                changeSprite(spritesPtns.at(movePatternLeft)[0]);
+                break;
+                case DOWN:
+                    /*if(!collide(map,EntityType::PLATFORM,DOWN))
+                        movement.y += playerSpeed;*/
+                    break;
+                case LEFT: {
+                    if (lastDirection != LEFT) {
+                        TimeAnimation = 0;
+                    } else {
+                        TimeAnimation += elapsedTime.asMilliseconds();
+                    }
+                    if (playerState != FALLING) {
+                        changeSprite(updateAnimation(&TimeAnimation, 100, spritesPtns.at(movePatternLeft)));
+                    } else {
+                        changeSprite(spritesPtns.at(movePatternLeft)[0]);
+                    }
+                    //printf("%d |",elapsedTime.asMilliseconds());
+                    sprite.move(moveLeft * elapsedTime.asSeconds());
+                    if (map.collide(sprite, EntityType::PLATFORM, RIGHT))
+                        sprite.move(moveRight * elapsedTime.asSeconds());
+                    lastDirection = LEFT;
+                    break;
+                }
+                case RIGHT:
+                    if (lastDirection != RIGHT) {
+                        TimeAnimation = 0;
+                    } else {
+                        TimeAnimation += elapsedTime.asMilliseconds();
+                    }
+                //printf("%d |",elapsedTime.asMilliseconds());
+
+                if (playerState != FALLING) {
+
+                    changeSprite(updateAnimation(&TimeAnimation, 100, spritesPtns.at(movePatternRight)));
+                } else {
+                    changeSprite(spritesPtns.at(movePatternRight)[0]);
+                }
+                sprite.move(moveRight * elapsedTime.asSeconds());
+                if (map.collide(sprite, EntityType::PLATFORM, RIGHT))sprite.move(moveLeft * elapsedTime.asSeconds());
+                lastDirection = RIGHT;
+                break;
+                default:
+                    if (playerState != JUMP && playerState != STARTJUMP && playerState != FALLING) {
+
+                        if (lastDirection == LEFT) {
+
+                        }
+                        if (lastDirection == LEFT) {
+                            changeSprite(spritesPtns.at(movePatternLeft)[0]);
+                        }
+                        lastDirection = NONE;
+                    }
             }
-            lastDirection = NONE;
-        }
-    }
-    switch (playerState)
-    {
-        case JUMP:
-            sprite.move(moveJump * elapsedTime.asSeconds());
-            if (!collide(map, EntityType::PLATFORM, RIGHT))
-            if(!map.collide(sprite,EntityType::PLATFORM,RIGHT))
-            {
-                changeSprite(spritesPtns.at(jumpPatternLeft)[0]);
-                jumpValue -= MARIO_JUMP_SPEED;
-            } else
-            {
-                sprite.move(moveNotJump * elapsedTime.asSeconds());
-                jumpValue = 0;
+            switch (playerState) {
+                case JUMP:
+                    sprite.move(moveJump * elapsedTime.asSeconds());
+                    if (!map.collide(sprite, EntityType::PLATFORM, RIGHT)) {
+                        changeSprite(spritesPtns.at(jumpPatternLeft)[0]);
+                        jumpValue -= MARIO_JUMP_SPEED;
+                    } else {
+                        sprite.move(moveNotJump * elapsedTime.asSeconds());
+                        jumpValue = 0;
+                    }
+                    if (jumpValue <= 0) {
+                        playerState = IDLE;
+                    }
+                case FALLING:
+                    changeSprite(spritesPtns.at(jumpPatternLeft)[0]);
+                case IDLE:
+                    //changeSprite(spritesPtns.at(movePatternLeft)[0]);
+                default:;
+
             }
-            if (jumpValue <= 0)
-            {
-                playerState = IDLE;
-            }
-        case FALLING:
-            changeSprite(spritesPtns.at(jumpPatternLeft)[0]);
-        case IDLE:
-            //changeSprite(spritesPtns.at(movePatternLeft)[0]);
-        default:;
 
     }
     direction = NONE;
